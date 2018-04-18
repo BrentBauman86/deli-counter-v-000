87134b0ce244104bf6f06b5a676b6b1a733f279f
Brent Bauman <brentbauman86@gmail.com>
2018-04-18 13:37:51 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,13 +4,13 @@ def line(names)
 if names.length == 0
   puts "The line is currently empty."
 else
-phrase = []
-names.each.with_index(1) {|name, number| phrase << " #{number}. #{name}"}
-puts "The line is currently:#{phrase.join}"
-end
+  phrase = []
+    names.each.with_index(1) {|name, number| phrase << " #{number}. #{name}"}
+      puts "The line is currently:#{phrase.join}"
+  end
 end
 
 def take_a_number(current, new)
-  current.unshift("new")
+  current.unshift("Ada")
     puts "Welcome, #{new}. You are number 1 in line."
 end
