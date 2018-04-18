da54698f5caec755a271300792b40bc3caca8f8d
Brent Bauman <brentbauman86@gmail.com>
2018-04-18 14:16:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,10 +13,10 @@ end
 def take_a_number(current, new)
   if current.length == 0
       current.unshift("Ada")
-        puts "Welcome, #{new}. You are number 1 in line."
+        puts "Welcome, #{new}. You are #{current.length} in line."
   else
     current.length > 1
       current.push("Grace")
-        puts "Welcome, #{new}. You are number 4 in line."
+        puts "Welcome, #{new}. You are #{current.length} in line."
 end
 end
