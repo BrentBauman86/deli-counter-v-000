3dc9c28d355c666d8709838a46d7cebd72005a38
Brent Bauman <brentbauman86@gmail.com>
2018-04-18 13:36:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,16 @@
 katz_deli = []
 
-def line(katz_deli)
-if katz_deli == 0
+def line(names)
+if names.length == 0
   puts "The line is currently empty."
 else
-  puts "You're next in line."
+phrase = []
+names.each.with_index(1) {|name, number| phrase << " #{number}. #{name}"}
+puts "The line is currently:#{phrase.join}"
+end
 end
 
-def take_a_number()
+def take_a_number(current, new)
+  current.unshift("new")
+    puts "Welcome, #{new}. You are number 1 in line."
 end
